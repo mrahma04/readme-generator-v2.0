b64ace67ca61838e45c5f6658f74d43105c339eb
Mohammad Rahman <mbrahman@gmail.com>
2022-03-20 15:43:41 -0400

    finished

--- index.js
+++ index.js
@@ -1,67 +1,90 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
+const fs = require('fs')
+
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-// const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    },
+    {
+        type: 'list',
+        name: 'licenseName',
+        message: 'Choose a license for your project:',
+        choices: ['MIT License', 'GPL']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter license holder name:'
+    },
+]
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            if (err) {
+                reject(err)
+                return
+            }
 
-// TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-// init();
-
-const initialPrompt = async () => {
-    const answers = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?'
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Enter a brief description:'   
-        },
-        {
-            type: 'editor',
-            name: 'installation',
-            message: 'Enter installation instructions:'   
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Enter usage information:'
-        },
-        {
-            type: 'input',
-            name: 'contributions',
-            message: 'Enter contribution guidelines:'
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'Enter test instructions:'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter your email address:'
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'Choose a license for your project:',
-            choices: ['MIT License', 'GPL']
-        }
-    ])
-
-    console.log(answers)
+            resolve({
+                ok: true,
+                message: 'File created!'
+            })
+        })
+    })
 }
 
-initialPrompt()
+// TODO: Create a function to initialize app
+async function init() {
+    const answers = await inquirer.prompt(questions)
+    const getText = generateMarkdown(answers)
+    writeToFile('./dIst/README.md', getText)
+}
+
+// Function call to initialize app
+init();
 
 // Title
 // Description
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,74 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if (license === '') {
+    return ``
+  }
+  if (license === 'MIT License') {
+    return `![MIT License](https://img.shields.io/badge/license-MIT-green)`
+  }
+  if (license === 'GPL') {
+    return `![GNU GPLv3](https://img.shields.io/badge/license-GPL-blue)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+  if (license === '') {
+    return ``
+  }
+  if (license === 'MIT License') {
+    return `[MIT](https://choosealicense.com/licenses/mit/)`
+  }
+  if (license === 'GPL') {
+    return `[GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/)`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `Copyright (c) ${license.name}. All rights reserved.
+Licensed under the ${renderLicenseLink(license.licenseName)} license.
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.licenseName)}
 
-`;
+## ${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseSection(data)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.test}
+
+## Questions
+My [GitHub](https://github.com/${data.github})
+For questions or comments, please email ${data.email}
+`
 }
 
 module.exports = generateMarkdown;
