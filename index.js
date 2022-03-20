3ff8abb749f870c7448d9a7a2d655418ec43a34b
Mohammad Rahman <mbrahman@gmail.com>
2022-03-20 08:13:16 -0400

    created initial files

---
+++ index.js
@@ -0,0 +1,68 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+
+// TODO: Create an array of questions for user input
+// const questions = [];
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+// function init() {}
+
+// Function call to initialize app
+// init();
+
+const initialPrompt = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a brief description:'   
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions:'   
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information:'
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'Enter contribution guidelines:'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter test instructions:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address:'
+        }
+    ])
+
+    console.log(answers)
+}
+
+initialPrompt()
+
+// Title
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions
